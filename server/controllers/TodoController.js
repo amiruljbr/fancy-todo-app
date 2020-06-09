e1323212fa265a6f8258ec6504fd36869dccbb4c
amiruljbr <amiruljabar@gmail.com>
2020-06-09 18:06:53 +0700

    add route edit

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -39,6 +39,32 @@ class TodoController {
       res.status(500).json(err)
     })
   }
+
+  static editPost(req,res){
+    let dataTodo = {
+      id:req.params.id,
+      title:req.body.title,
+      description:req.body.description,
+      due_date:req.body.due_date,
+      UserId:req.userData.id
+    }
+
+    Todo.update({
+      title:dataTodo.title,
+      description:dataTodo.description,
+      due_date:dataTodo.due_date
+    }, {
+      where: {
+        id: dataTodo.id
+      }
+    })
+    .then(data=>{
+      res.status(200).json(data)
+    })
+    .catch(err=>{
+      res.status(500).json(err)
+    })
+  }
 }
 
 module.exports = TodoController;
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -6,5 +6,6 @@ router.use(authentication)
 router.get('/',TodoController.getAllTodo);
 router.post('/create',TodoController.added);
 router.delete('/delete/:id',authorization,TodoController.delete);
+router.post('/edit/:id',authorization,TodoController.editPost);
 
 module.exports = router;
